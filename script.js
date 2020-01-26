83f6b0f318b1ee466d8c282da47c42d190c9c7d3
Evan Yang <evanyang@pop-os.localdomain>
2020-01-26 01:04:28 -0800

    added some text, buttons, quiz object

---
+++ script.js
@@ -0,0 +1,34 @@
+let quiz = [
+  {
+    id: 0,
+    question: "What is JavaScript?",
+    answer_choices: [
+      "Same as Java",
+      "The main programming language of the browser",
+      "The main programming language of computer systems",
+      "A set of rules about styling your HTML"
+    ],
+    correct_answer: 1 // count from 0
+  },
+  {
+    id: 1,
+    question: "Which is better practice, let/const or var?",
+    answer_choices: [
+      "Let/const",
+      "var",
+      "Both",
+      "Neither"
+    ],
+    correct_answer: 0
+  },
+  {
+    id: 2,
+    question: "What does document.getElementById() do?"
+    answer_choices: [
+      "Get an element by HTML element type",
+      "Get an element by class",
+      "Get an element by id"
+    ],
+    correct_answer: 2
+  }
+]
